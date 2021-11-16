327f019296329826b6a08633797f67e19f06000c
puraminy <pouramini@gmail.com>
2021-11-16 21:28:00 +0330

    commit

--- run_t5_mlm_flax.py
+++ run_t5_mlm_flax.py
@@ -1009,4 +1009,12 @@ if __name__ == "__main__":
                         #tokenizer.save_pretrained(training_args.output_dir)
                         #if training_args.push_to_hub:
                         #    repo.push_to_hub(commit_message=f"Saving weights and logs of step {cur_step}", blocking=False)
-                            
+        # save the model after training                    
+        save_checkpoint(
+            model,
+            training_args.output_dir,
+            state,
+            cur_step,
+            with_opt=True,
+            push_to_hub=training_args.push_to_hub
+        )
